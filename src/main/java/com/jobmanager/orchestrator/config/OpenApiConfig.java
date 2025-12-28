e990a4f4c38f4f16a9136fe977f14c21135b68f3
Roey Geffen <roeygol@gmail.com>
2025-12-28 15:59:27 +0200

    remove targer

---
+++ src/main/java/com/jobmanager/orchestrator/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.jobmanager.orchestrator.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration for OpenAPI/Swagger documentation.
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Job Orchestrator API")
+                        .version("1.0.0")
+                        .description("Production-grade job orchestration microservice that orchestrates job execution across downstream microservices")
+                        .contact(new Contact()
+                                .name("Job Manager Team")
+                                .email("support@jobmanager.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
+
